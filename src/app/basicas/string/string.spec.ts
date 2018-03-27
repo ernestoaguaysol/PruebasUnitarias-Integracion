7b3d3e6a3723e1b5029d52b173114fa8e4702174
Ernesto <ernestoaguaysol@gmail.com>
2018-03-27 11:43:47 -0300

    Pruebas unitarias basicas, coverage, test

---
+++ src/app/basicas/string/string.spec.ts
@@ -0,0 +1,25 @@
+import { mensaje } from "./string";
+
+// describe();
+
+describe('Pruebas de strings', () => {
+
+
+
+    it('Debe de regresar un string', () => {
+        const resp = mensaje('Fernando');
+
+        expect( (typeof resp) ).toBe('string');
+
+    });
+
+    it('Debe de regresar un saludo con el nombre enviado', () => {
+
+        const nombre = 'Juan';
+
+        const resp = mensaje(nombre);
+
+        expect( resp ).toContain(nombre);
+
+    });
+});
